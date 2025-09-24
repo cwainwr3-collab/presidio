f5864766e764902d14d04148c177baa6bc33c79f
Christopher Wainwright <231749634+cwainwr3-collab@users.noreply.github.com>
2025-09-24 21:27:18 +0000

    fixing our output and sample

--- presidio-anonymizer/sample.py
+++ presidio-anonymizer/sample.py
@@ -9,11 +9,8 @@ engine = AnonymizerEngine()
 # Operators to get the anonymization output:
 result = engine.anonymize(
     text="My name is cwainwr3-collab, cwainwr3-collab",
-    analyzer_results=[
-        RecognizerResult(entity_type="PERSON", start=11, end=15, score=0.8),
-        RecognizerResult(entity_type="PERSON", start=17, end=27, score=0.8),
-    ],
-    operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})},
+    analyzer_results=[],
+    operators={"PERSON": {"type": "replace", "new_value": "BIP"}}
 )
 
 print(result)
